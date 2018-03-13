fda8f2f27f9b2e9bb41317d1baf08e57beaa9879
jubin <jubin@gmail.com>
2018-03-13 21:49:18 +0530

    second

---
+++ server/src/main/java/com/workout/repository/WorkoutTransactionsRepo.java
@@ -0,0 +1,15 @@
+package com.workout.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.workout.dao.WorkOutTransactions;
+
+public interface WorkoutTransactionsRepo extends CrudRepository<WorkOutTransactions, Integer> {
+	
+
+	@Query("SELECT WT.txnId,WT.startTime,WT.stopTime,WT.duration,WT.calBurnt,WT.workout.workoutId FROM WorkOutTransactions WT WHERE WT.workout.user.userId= :userId")
+	Iterable<WorkOutTransactions> findByWorkId(@Param("userId") Long userId);
+
+}
---
+++ server/src/main/java/com/workout/services/WorkoutTransactionService.java
@@ -0,0 +1,26 @@
+package com.workout.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workout.dao.WorkOutTransactions;
+import com.workout.repository.WorkoutTransactionsRepo;
+
+@Service
+public class WorkoutTransactionService {
+
+	@Autowired
+	private WorkoutTransactionsRepo workoutTransactionRepo;
+	
+	
+	public WorkOutTransactions updateWorkoutTxnDetails(WorkOutTransactions workoutTransactions) {
+		workoutTransactionRepo.save(workoutTransactions);
+		return workoutTransactions;
+	}
+	
+	public Iterable<WorkOutTransactions> getWorkoutTxnDetails(Long userId) {
+		Iterable<WorkOutTransactions> workoutTxnList=workoutTransactionRepo.findByWorkId(userId);
+		return workoutTxnList;
+	}
+
+}
